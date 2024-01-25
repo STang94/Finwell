13cf23283b191af4ef1094324326a0af0155e8e4
Hugo RAVAILHE <hugo.rh@free.fr>
2024-01-25 01:05:02 +0100

    bank d'image

---
+++ Camera/picture.py
@@ -0,0 +1,16 @@
+import cv2
+import time
+
+# Open the webcam
+cap = cv2.VideoCapture(0)
+
+
+ret, frame = cap.read()
+
+# Save the image
+
+cv2.imwrite(f'image75.png', frame)
+
+
+# Release the webcam
+cap.release()
