886920fda063ce7b162eba2b4a79b25c38f2c76a
STang94 <stephane.tang@efrei.net>
2024-02-01 10:36:20 +0100

    sms

---
+++ sms.py
@@ -0,0 +1,28 @@
+import requests
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+servicePlanId = getenv("SERVICEPLANID")
+apiToken = getenv("APITOKEN")
+sinchNumber = getenv("SINCHNUMBER")
+toNumber = getenv("TONUMBER")
+url = "https://us.sms.api.sinch.com/xms/v1/" + servicePlanId + "/batches"
+
+payload = {
+  "from": sinchNumber,
+  "to": [
+    toNumber
+  ],
+  "body": "Hello how are you"
+}
+
+headers = {
+  "Content-Type": "application/json",
+  "Authorization": "Bearer " + apiToken
+}
+
+response = requests.post(url, json=payload, headers=headers)
+
+data = response.json()
+print(data)
